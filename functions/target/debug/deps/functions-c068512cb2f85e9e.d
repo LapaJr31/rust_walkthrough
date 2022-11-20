7494cd540bb129e27e4b10316cf37bb6c366b94f
Yar Lat <jarlat04@gmail.com>
2022-11-20 16:34:23 +0100

    did some stuff today but not very productive

---
+++ functions/target/debug/deps/functions-c068512cb2f85e9e.d
@@ -0,0 +1,5 @@
+/home/ylss/Documents/Projects/rust/rust_walkthrough/functions/target/debug/deps/functions-c068512cb2f85e9e: src/main.rs
+
+/home/ylss/Documents/Projects/rust/rust_walkthrough/functions/target/debug/deps/functions-c068512cb2f85e9e.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/ylss/Documents/Projects/rust/rust_walkthrough/functions/target/debug/functions: /home/ylss/Documents/Projects/rust/rust_walkthrough/functions/src/main.rs
